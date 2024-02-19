f13f8cad29274b2cfee3c312070b2dc08edec9a1
Amy <alchung1@uw.edu>
2024-02-19 01:11:30 -0800

    rename column

--- info 201 final project.R
+++ info 201 final project.R
@@ -3,3 +3,6 @@ food_df <- read.csv("dietary-compositions-by-commodity-group.csv")
 mental_illness_df <- read.csv("mental-illness-estimated-cases.csv")
 
 food_mental_df <- left_join(mental_illness_df, food_df)
+
+#rename column
+colnames(food_mental_df)[1] = "Country/Region"
