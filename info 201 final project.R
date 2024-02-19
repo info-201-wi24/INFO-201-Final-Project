909f253348f5e89ca437ed2c66507eddcf376599
Amy <alchung1@uw.edu>
2024-02-19 00:41:57 -0800

    joins dataset

---
+++ info 201 final project.R
@@ -0,0 +1,5 @@
+food_df <- read.csv("dietary-compositions-by-commodity-group.csv")
+
+mental_illness_df <- read.csv("mental-illness-estimated-cases.csv")
+
+food_mental_df <- left_join(mental_illness_df, food_df)
