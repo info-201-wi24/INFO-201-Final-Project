2143952cab1143c96bfcc9bb183447303419f3b3
Amy <alchung1@uw.edu>
2024-02-19 03:04:48 -0800

    add columns

--- info 201 final project.R
+++ info 201 final project.R
@@ -2,7 +2,20 @@ food_df <- read.csv("dietary-compositions-by-commodity-group.csv")
 
 mental_illness_df <- read.csv("mental-illness-estimated-cases.csv")
 
+#join dataframes
 food_mental_df <- left_join(mental_illness_df, food_df)
 
-#rename column
-colnames(food_mental_df)[1] = "Country/Region"
+#create additional columns
+food_mental_df <- food_mental_df %>% 
+  mutate(Total.mental.disorders.cases = Current.number.of.cases.of.anxiety.disorders..in.both.sexes.aged.all.ages
+         + Current.number.of.cases.of.depressive.disorders..in.both.sexes.aged.all.ages +
+           Current.number.of.cases.of.schizophrenia..in.both.sexes.aged.all.ages +
+           Current.number.of.cases.of.bipolar.disorder..in.both.sexes.aged.all.ages + 
+           Current.number.of.cases.of.eating.disorders..in.both.sexes.aged.all.ages)
+
+#create summarization dataframe
+avg_sugar_df <- food_mental_df %>% 
+  group_by(Entity) %>%
+  summarize(avg.sugar.intake = mean(Daily.caloric.intake.per.person.from.sugar, na.rm = TRUE))
+  
+  
