ac71abbf4100610920e49db1869fc98836d7af16
Amy <alchung1@uw.edu>
2024-02-20 12:48:07 -0800

    add missing code

--- info 201 final project.R
+++ info 201 final project.R
@@ -1,3 +1,5 @@
+library(dplyr) 
+
 food_df <- read.csv("dietary-compositions-by-commodity-group.csv")
 
 mental_illness_df <- read.csv("mental-illness-estimated-cases.csv")
---
+++ practice.R
@@ -0,0 +1,1 @@
+read.csv("diet-with-mental-illness-cases.csv")
